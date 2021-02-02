11cb521fcd7aa828bd70e7bad5567b758ba46c78
jasperwreed <jmixon.reed@gmail.com>
2021-02-02 11:59:47 -0800

    began login and register pages

---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LoginContainer extends Component {
+  render() {
+    return(
+      <>
+        <h1>Welcome To Login Page</h1>
+      </>
+    )
+  }
+}
---
+++ src/containers/RegisterContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RegisterContainer extends Component {
+  render() {
+    return (
+      <>
+        <h1>Create Account</h1>
+      </>
+    )
+  }
+}
